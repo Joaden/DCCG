7ac9c04b87cc41d197c4d5ccbde597debd41ea3f
DeniC <chanet.denis@hotmail.fr>
2021-10-28 17:13:13 +0200

    save Authentication

---
+++ migrations/Version20211028131009.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211028131009 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE articles DROP FOREIGN KEY FK_BFDD3168BCF5E72D');
+        $this->addSql('DROP INDEX IDX_BFDD3168BCF5E72D ON articles');
+        $this->addSql('ALTER TABLE articles CHANGE date date DATETIME NOT NULL, CHANGE categorie categorie_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE articles ADD CONSTRAINT FK_BFDD3168BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categories (id)');
+        $this->addSql('CREATE INDEX IDX_BFDD3168BCF5E72D ON articles (categorie_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE articles DROP FOREIGN KEY FK_BFDD3168BCF5E72D');
+        $this->addSql('DROP INDEX IDX_BFDD3168BCF5E72D ON articles');
+        $this->addSql('ALTER TABLE articles CHANGE date date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CHANGE categorie_id categorie INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE articles ADD CONSTRAINT FK_BFDD3168BCF5E72D FOREIGN KEY (categorie) REFERENCES categories (id)');
+        $this->addSql('CREATE INDEX IDX_BFDD3168BCF5E72D ON articles (categorie)');
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Articles;
 use App\Entity\Categories;
 use App\Entity\Users;
+use App\Entity\User;
 
 class HomeController extends AbstractController
 {
@@ -18,14 +19,17 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         $repo = $this->getDoctrine()->getRepository(Articles::class);
+        $repoCategories = $this->getDoctrine()->getRepository(Categories::class);
 
         $articles = $repo->findAll();
+        $categories = $repoCategories->findAll();
 
-//        dd($articles);
+//       dd($articles);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'Home Page Articles',
             'articles' => $articles,
+            'categories' => $categories,
 
         ]);
     }
