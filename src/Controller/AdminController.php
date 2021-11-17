60c1844bd9424188c623215a460afc79165e95bb
DeniC <chanet.denis@hotmail.fr>
2021-11-17 02:47:34 +0100

    manage users refonte

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/admin", name="admin_")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+
+    /**
+     * Liste les utilisateurs du site
+     *
+     * @Route("/users", name="users")
+     */
+    public function usersList(UserRepository $users){
+
+        return $this->render('admin/index.html.twig', [
+            'users' => $users->findAll()
         ]);
     }
 }
